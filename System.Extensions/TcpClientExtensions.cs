c81d13c7b97cf6bed7dce5e2e8b479cafc42bf56
Andrius Bentkus <andrius.bentkus@gmail.com>
2015-03-17 18:17:29 +0100

    Add more overload methods to TcpClient.
    We have an overload on IPHostEntry since
    Connect accepts a list of ip addresses.

--- System.Extensions/TcpClientExtensions.cs
+++ System.Extensions/TcpClientExtensions.cs
@@ -6,9 +6,24 @@ namespace System.Net.Sockets
 {
 	public static class TcpClientExtensions
 	{
+		public static void Connect(this TcpClient client, IPEndPoint ipEndPoint)
+		{
+			client.Connect(ipEndPoint.Address, ipEndPoint.Port);
+		}
+
+		public static void Connect(this TcpClient client, IPHostEntry ipHostEntry, int port)
+		{
+			client.Connect(ipHostEntry.AddressList, port);
+		}
+
 		public static async Task ConnectAsync(this TcpClient client, IPEndPoint ipEndPoint)
 		{
 			await client.ConnectAsync(ipEndPoint.Address, ipEndPoint.Port);
+		}
+
+		public static async Task ConnectAsync(this TcpClient client, IPHostEntry ipHostEntry, int port)
+		{
+			await client.ConnectAsync(ipHostEntry.AddressList, port);
 		}
 	}
 }
