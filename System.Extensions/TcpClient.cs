ba4d9fadbb67683334ab365f7b1b579f17155955
Andrius Bentkus <andrius.bentkus@gmail.com>
2015-03-17 12:16:23 +0100

    Add TcpClient overload for ConnectAsync.
    Jesus christ, do I have to really type
    ConnectAsync(ipEndpoint.Address, ipEndPoint.Port) out?

---
+++ System.Extensions/TcpClient.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace System.Net.Sockets
+{
+	public static class TcpClientExtensions
+	{
+		public static async Task ConnectAsync(this TcpClient client, IPEndPoint ipEndPoint)
+		{
+			await client.ConnectAsync(ipEndPoint.Address, ipEndPoint.Port);
+		}
+	}
+}
+
