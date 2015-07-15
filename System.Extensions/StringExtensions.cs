63f2e5e17e77e245651142314cc1d05455f93345
Andrius Bentkus <andrius.bentkus@gmail.com>
2015-07-15 14:45:18 +0200

    Add StringExtensions.

---
+++ System.Extensions/StringExtensions.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace System
+{
+	public static class StringExtensions
+	{
+		#region StartsWith
+
+		public static bool StartsWith(this string str, params string[] strings)
+		{
+			return StartsWith(str, strings as IEnumerable<string>);
+		}
+
+		public static bool StartsWith(this string str, IEnumerable<string> strings)
+		{
+			return strings.Select(value => str.StartsWith(value)).Contains(true);
+		}
+
+		#endregion
+
+		#region SkipPrefix
+
+		public static string SkipPrefix(this string str, string prefix)
+		{
+			if (str.StartsWith(prefix)) {
+				return str.Substring(prefix.Length);
+			} else {
+				return str;
+			}
+		}
+
+		public static string SkipPrefix(this string str, params string[] prefixes)
+		{
+			foreach (var prefix in prefixes) {
+				if (str.StartsWith(prefix)) {
+					return str.Substring(prefix.Length);
+				}
+			}
+			return str;
+		}
+
+		#endregion
+	}
+}
+
