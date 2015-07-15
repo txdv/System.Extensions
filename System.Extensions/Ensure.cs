7572d8a2cf4de5f92caf651f5a0ca30c0e9da0c1
Andrius Bentkus <andrius.bentkus@gmail.com>
2015-07-15 14:24:26 +0200

    Add Positions extensions.

---
+++ System.Extensions/Ensure.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace System.Extensions
+{
+	internal static class Ensure
+	{
+		public static void ArgumentNotNull(object value, string name)
+		{
+			if (value == null) {
+				throw new ArgumentNullException(name);
+			}
+		}
+
+		public static void NotSupported<T, TClass>(string message)
+		{
+			if (typeof(T) is TClass) {
+				throw new NotSupportedException(typeof(TClass).ToString() + " is not supported");
+			}
+		}
+
+		public static void NotSupported<T, TClass>()
+		{
+			NotSupported<T, TClass>(typeof(TClass).ToString() + " is not supported");
+		}
+	}
+}
+
---
+++ System.Extensions/PositionsExtensions.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace System.Extensions
+{
+	public static class PositionsExtension
+	{
+		/// <summary>
+		/// Returns the first position of an element specified by the predicate.
+		/// </summary>
+		/// <param name="source">Source.</param>
+		/// <param name="predicate">Predicate.</param>
+		/// <typeparam name="TSource">The 1st type parameter.</typeparam>
+		public static int Position<TSource>(this IEnumerable<TSource> source, Func<TSource, bool> predicate)
+		{
+			return Positions<TSource>(source, predicate).FirstOrDefault();
+		}
+
+		/// <summary>
+		/// Returns the first position of an element present in the elements argument.
+		/// </summary>
+		/// <param name="source">Source.</param>
+		/// <param name="elements">Elements.</param>
+		/// <typeparam name="TSource">The 1st type parameter.</typeparam>
+		public static int Position<TSource>(this IEnumerable<TSource> source, IEnumerable<TSource> elements)
+		{
+			return source.Position(elements.Contains);
+		}
+
+		/// <summary>
+		/// Returns the first position of an element present in the elements argument.
+		/// </summary>
+		/// <param name="source">Source.</param>
+		/// <param name="elements">Elements.</param>
+		/// <typeparam name="TSource">The 1st type parameter.</typeparam>
+		public static int Position<TSource>(this IEnumerable<TSource> source, params TSource[] elements)
+		{
+			return source.Position(elements as IEnumerable<TSource>);
+		}
+
+		/// <summary>
+		/// Returns the positions of the elements in an IEnumerable specified by the predict.
+		/// </summary>
+		/// <param name="source">Source.</param>
+		/// <param name="predicate">Predicate to determine whether to return the position of an element in the source or not.</param>
+		/// <typeparam name="TSource">The 1st type parameter.</typeparam>
+		public static IEnumerable<int> Positions<TSource>(this IEnumerable<TSource> source, Func<TSource, bool> predicate)
+		{
+			Ensure.NotSupported<TSource, System.Collections.IDictionary>();
+			Ensure.ArgumentNotNull(source, nameof(source));
+			Ensure.ArgumentNotNull(predicate, nameof(predicate));
+
+			int i = 0;
+			foreach (var element in source) {
+				if (predicate(element)) {
+					yield return i;
+				}
+				i++;
+			}
+		}
+
+		/// <summary>
+		/// Returns the positions of the elements present in the elements argument.
+		/// </summary>
+		/// <param name="source">Source.</param>
+		/// <param name="elements">Elements.</param>
+		/// <typeparam name="TSource">The 1st type parameter.</typeparam>
+		public static IEnumerable<int> Positions<TSource>(this IEnumerable<TSource> source, IEnumerable<TSource> elements)
+		{
+			return source.Positions(elements.Contains);
+		}
+
+		/// <summary>
+		/// Returns the positions of the elements present in the elements argument.
+		/// </summary>
+		/// <param name="source">Source.</param>
+		/// <param name="elements">Elements.</param>
+		/// <typeparam name="TSource">The 1st type parameter.</typeparam>
+		public static IEnumerable<int> Positions<TSource>(this IEnumerable<TSource> source, params TSource[] elements)
+		{
+			return source.Positions(elements as IEnumerable<TSource>);
+		}
+	}
+}
+
