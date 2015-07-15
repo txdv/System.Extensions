5adaf3f8fcfcbd5fcd51b402657e2a82e070847e
Andrius Bentkus <andrius.bentkus@gmail.com>
2015-07-15 13:06:09 +0200

    Add CombinationExtensions.
    Returns all combinations of an IEnumerable.

---
+++ System.Extensions/CombinationExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace System.Extensions
+{
+	public static class CombinationExtensions
+	{
+		public static IEnumerable<IEnumerable<T>> Combinations<T>(this IEnumerable<T[]> arrays)
+		{
+			if (!arrays.Any()) {
+				yield return Enumerable.Empty<T>();
+				yield break;
+			}
+
+			foreach (var element in arrays.First()) {
+				foreach (var combintation in Combinations(arrays.Skip(1))) {
+					yield return new[] { element }.Concat(combintation);
+				}
+			}
+		}
+	}
+}
+
